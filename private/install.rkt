68a8948915daedf91ba97863287e0c2b49f877c4
David Wilson <davidwil@posteo.de>
2020-10-04 20:57:37 +0100

    Got c code building correctly

--- private/install.rkt
+++ private/install.rkt
@@ -9,7 +9,8 @@
 (define (pre-installer collections-top-path racl-path)
   (define private-path (build-path racl-path "private"))
   (define unpacked-path (path->string (build-path private-path "gnulib")))
-  (define gnulib-objects (path->string (build-path private-path "gnulib" "*.o")))
+  (define gnulib-path (path->string (build-path unpacked-path "gllib")))
+  (define gnulib-objects (path->string (build-path gnulib-path  "*.o")))
 
   (define fadvise-path (path->string (build-path private-path "fadvise" "fadvise.c")))
   (define fadvise-output-path (path->string (build-path private-path "fadvise.o")))
@@ -21,12 +22,10 @@
   
   
   (untgz (build-path private-path "gnulib.tgz") #:dest private-path)
-  (system (string-append "cd " unpacked-path " && ./configure && make"))
-  #;(system (string-append
-           "gcc "unpacked-path"/gllib/*.o -I "unpacked-path" -shared -o "output-path))
+  (system (string-append "cd " unpacked-path " && ./configure && make CFLAGS='-fPIC'"))
   (system (string-append "gcc -c -fPIC "fadvise-path" -o "fadvise-output-path))
-  (system (string-append "gcc -c -fPIC "sum-path" -o"sum-output-path" -I"unpacked-path))
-  (system (string-append "gcc "sum-output-path" "fadvise-output-path" "gnulib-objects" -shared -o "output-path" -I"unpacked-path))
+  (system (string-append "gcc -c -fPIC "sum-path" -o"sum-output-path" -I"gnulib-path))
+  (system (string-append "gcc "sum-output-path" "fadvise-output-path" "gnulib-objects" -shared -o "output-path" -I"gnulib-path))
   
   (delete-directory/files unpacked-path))
 
